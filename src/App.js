fa2736b7a6d699328821c762f07e2dd66aa96d95
marissa.ana <marissa.ana07@gmail.com>
2024-09-05 12:54:50 +0700

    Penambahan Section pada Halaman Utama

--- src/App.js
+++ src/App.js
@@ -6,6 +6,17 @@ function App() {
       <header className="App-header">
         <p>Marissa Ana</p>
       </header>
+
+      <main>
+        <section>
+          <h2>Welcome to Marissa Ana's Page</h2>
+          <p>
+            This is a new section added to the app. Here you can find more
+            information about Marissa Ana and my journey in learning Git and
+            GitHub.
+          </p>
+        </section>
+      </main>
     </div>
   );
 }
